30efaaa2b28bb49cffbd1de810a56571672ccae5
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2023-09-05 19:50:20 -0300

    Filtros y Details, Funcionando.

--- js/past_events.js
+++ js/past_events.js
@@ -1,32 +1,88 @@
-const $rowCards = document.getElementById("rowCards");
 const events = data.events;
+const $rowCards = document.getElementById("rowCards");
+const $checkDiv = document.getElementById("check");
+const $inputSearch = document.getElementById("search");
+const catSinRep = [ ... new Set(events.map(objet => objet.category)) ];
 const fechaActual = Date.parse(data.currentDate);
-for (let event of events) {
-    if (Date.parse(event.date)<=fechaActual) {
-        $rowCards.innerHTML += `
-        <div class="col d-flex align-items-stretch">
-        <div class="card bg-light p-2">
-        <img
-        src="${event.image}"
-        class="card-img-top"
-        alt="${event.name}"
-        />
-        <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text">
-          ${event.description}
-        </p>
-        <p><b>Date :</b> ${event.date}</p>
-        <div
-          class="d-flex justify-content-between align-items-center"
-        >
-          <h4>Price $ ${event.price}</h4>
-          <button class="btn btn-primary" type="submit">
-            <a href="details.html">Details</a>
-          </button>
-        </div>
-        </div>
-        </div>
-        </div>`; 
-    }
+let eventosFiltrados = events.filter(objet =>  Date.parse(objet.date) <= fechaActual );
+
+function templateCards(objet) {
+  let template ="";
+  template = `
+  <div class="col d-flex align-items-stretch">
+  <div class="card bg-light p-2">
+  <img
+  src="${objet.image}"
+  class="card-img-top"
+  alt="${objet.name}"
+  />
+  <div class="card-body">
+  <h5 class="card-title">${objet.name}</h5>
+  <p class="card-text">
+    ${objet.description}
+  </p>
+  <p><b>Date :</b> ${objet.date}</p>
+  <div
+    class="d-flex justify-content-between align-items-center"
+  >
+    <h4>Price $ ${objet.price}</h4>
+    <a href="details.html?id=${objet._id}" class="btn btn-primary">Details</a>
+  </div>
+  </div>
+  </div>
+  </div>`;
+  return template;
 }
+function imprimirCards(array,elemento){
+  let estructura = ""
+  array.forEach(objeto=>{
+    estructura += templateCards(objeto);
+  })
+  elemento.innerHTML = estructura;
+}
+
+imprimirCards(eventosFiltrados,$rowCards);
+
+function templateCheck(string){
+  let template = "";
+  template = `
+  <div class="form-check mx-3">
+  <input
+    class="form-check-input"
+    type="checkbox"
+    value="${string}"
+    id="${string}"
+  />
+  <label class="form-check-label" for="${string}">
+    ${string}
+  </label>
+</div>
+  `;
+  return template;
+}
+function imprimirCheck(array,elemento){
+  let estructura = "";
+  array.forEach(categoria =>{
+    estructura += templateCheck(categoria);
+  })
+  elemento.innerHTML = estructura;
+}
+imprimirCheck(catSinRep,$checkDiv);
+$checkDiv.addEventListener("change",(e)=>{
+  let nodeList = document.querySelectorAll("input[type='checkbox']:checked");
+  let arrayValores = Array.from(nodeList).map(objet => objet.value);
+  let objetosFiltrados = eventosFiltrados.filter(objet => arrayValores.includes(objet.category));
+  imprimirCards(objetosFiltrados,$rowCards);
+  if (objetosFiltrados.length == 0) {
+    imprimirCards(eventosFiltrados,$rowCards);
+  }
+})
+
+$inputSearch.addEventListener("keyup", (e)=>{
+  let inputText = e.target.value.toLowerCase();
+  let objetosFiltrados = eventosFiltrados.filter(objet => objet.name.toLowerCase().includes(inputText));
+  imprimirCards(objetosFiltrados,$rowCards);
+  if (inputText.length == 0) {
+    imprimirCards(eventosFiltrados,$rowCards);
+  }
+})
